b53455e362390d16151a76837b55f181114f6242
Maria <marialuizagodoy.senai@gmail.com>
2025-05-21 17:22:19 -0300

    Término da Fase 5 –Update interface AirportRepository – Adicionado assinatura de método Airport findByIataCode(). -Update classe AirportService -Adicionado método findByIataCodeSearch. -Update classe AirportController – Adicionado endpoint iatacode/{iataCode}.

--- src/main/java/local/godoy/airports/service/AirportService.java
+++ src/main/java/local/godoy/airports/service/AirportService.java
@@ -41,4 +41,10 @@ public class AirportService {
         
         return resultDTO;
     }
+    
+    public Airport findByIataCode(String iataCode) {
+        Airport result = airportRepository.findByIataCode(iataCode);
+        return result;
+        
+    }
 }
